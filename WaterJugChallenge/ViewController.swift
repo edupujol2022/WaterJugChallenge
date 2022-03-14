c9288e7dd2fd8f2052d2046d1f5ece1b574ebecb
Lic. Carlos Peralta <carlos.peralta@gmail.com>
2022-03-14 16:53:35 -0300

    Initial Commit

---
+++ WaterJugChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaterJugChallenge
+//
+//  Created by user on 14/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
